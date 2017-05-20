657cd5a8bb3e9a3a5970ffa64ddc97a4a32e2a7d
Tyler Davis <tyjdavis@outlook.com>
2017-05-20 00:08:47 -0400

    chat working box updating and sending to fb

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -4,27 +4,77 @@ var $ = window.jQuery = require('jquery');
 import materialize from 'materialize-css';
 window.Vel = require('materialize-css/js/velocity.min')
 import base from '../rebase';
+var firebase = require('firebase');
 
 window.base = base;
 
 class ChatBox extends Component {
 
-componentDidMount() {
+  constructor(props, context) {
+    super(props, context)
+    this.updateMessage = this.updateMessage.bind(this)
+    this.submitMessage = this.submitMessage.bind(this)
+    this.state = {
+      message: '',
+      messages: []
+    }
+  }
 
+
+componentDidMount() {
   $('.modal').modal();
 
+  firebase.database().ref('messages/').on('value', (snapshot) =>{
+
+    const currentMessages = snapshot.val()
+
+    if (currentMessages != null){
+      this.setState({
+        messages: currentMessages
+      })
+    }
+  })
 }
 
+updateMessage(event) {
+  this.setState({
+    message: event.target.value
+  })
+}
+
+submitMessage(event) {
+  const nextMessage = {
+    id: this.state.messages.length,
+    text: this.state.message
+  }
+
+  firebase.database().ref('messages/'+nextMessage.id).set(nextMessage)
+  // let list = Object.assign([], this.state.messages)
+  // list.push(nextMessage)
+  // this.setState({
+  //   messages: list
+  // })
+}
 
   render() {
-    console.log(this.props.user),
-    console.log(this.props.currentUser)
+    // console.log(this.props.user),
+    // console.log(this.props.currentUser)
+
+    const currentMessage = this.state.messages.map((message, i) => {
+      return(
+        <li key={message.id}>{message.text}</li>
+      )
+    })
 
     return (
       <div className='container'>
-        <h4>{this.props.user.name}</h4>
-        <input type="text" placeholder="Message" id="message"/>
-        <button className="waves-effect waves-light btn" id="message-button" type="submit">Submit</button>
+        <h5 className='center-align'>{this.props.user.name}</h5>
+          <ol>
+            {currentMessage}
+          </ol>
+        <input onChange={this.updateMessage} type="text" placeholder="Message" id="message"/>
+        <br />
+        <button onClick={this.submitMessage} className="waves-effect waves-light btn" id="message-button" type="submit">Submit</button>
       </div>
     )
   }
